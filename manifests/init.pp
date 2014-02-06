ca5ba862530809ed548d8f4b679ec9ab174ed4f3
Morgante Pell <morgante.pell@morgante.net>
2014-02-06 14:34:31 +0400

    spaces > tabs

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,16 +5,16 @@
 #   include docker
 
 class docker(
-	$boot2docker = 'https://raw.github.com/steeve/boot2docker/master/boot2docker'
+  $boot2docker = 'https://raw.github.com/steeve/boot2docker/master/boot2docker'
 ) {
-	include boxen::config
+  include boxen::config
 
-	file { "boot2docker":
-		name	=> "${boxen::config::bindir}/boot2docker",
-		ensure	=> present,
-		source	=> "${boot2docker}",
-		owner	=> root,
-		group	=> wheel,
-		mode	=> "ug+w"
-	}
+  file { 'boot2docker':
+    name    => "${boxen::config::bindir}/boot2docker",
+    ensure  => present,
+    source  => "${boot2docker}",
+    owner   => root,
+    group   => wheel,
+    mode    => 'ug+w'
+  }
 }
