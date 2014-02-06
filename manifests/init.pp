5b2db1df06f51ea42e9caea48d4e487343cd24d8
Morgante Pell <morgante.pell@morgante.net>
2014-02-06 14:31:19 +0400

    try downloading boot2docker

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,12 +2,19 @@
 #
 # Usage:
 #
-#   include meteorjs
+#   include docker
 
 class docker(
-  $boot2docker = 'https://raw.github.com/steeve/boot2docker/master/boot2docker'
+	$boot2docker = 'https://raw.github.com/steeve/boot2docker/master/boot2docker'
 ) {
-  include boxen::config
+	include boxen::config
 
-  anchor { 'Docker is here': }
+	file { "boot2docker":
+		name	=> "${boxen::config::bindir}/boot2docker",
+		ensure	=> present,
+		source	=> "${boot2docker}",
+		owner	=> root,
+		group	=> wheel,
+		mode	=> "ug+w"
+	}
 }
--- spec/classes/docker_spec.rb
+++ spec/classes/docker_spec.rb
@@ -4,6 +4,5 @@ require 'spec_helper'
 # or read http://rspec-puppet.com/tutorial/
 describe 'template' do
   it do
-    should contain_anchor('Hello_World')
   end
 end
